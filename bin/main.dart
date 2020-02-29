9a45e0789d4587910f29e00177028a851fcda642
Duncan Mak <duncan@yomigana>
2020-02-29 18:16:32 -0500

    New project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:parse_deriv/parse_deriv.dart' as parse_deriv;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parse_deriv.calculate()}!');
+}
---
+++ test/parse_deriv_test.dart
@@ -0,0 +1,8 @@
+import 'package:parse_deriv/parse_deriv.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
